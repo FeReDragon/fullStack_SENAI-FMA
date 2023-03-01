2398b4ec0e73e8812e3ee2b6184499c4cf980e83
“FeReDragon” <f3030ma@gmail.com>
2023-03-01 11:11:24 -0300

    [M1S05] Ex 1 - Solicite dois números

---
+++ Modulo_1/ex_S5/[M1S05] Ex 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DivisaoNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, resultado = 0;
+
+            try
+            {
+                Console.Write("Digite o primeiro número: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Digite o segundo número: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+                resultado = num1 / num2;
+
+                Console.WriteLine("Resultado da divisão:" + resultado);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Erro: não é possível dividir por zero.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erro: letras não podem ser informadas.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Erro: ocorreu um erro inesperado.");
+            }
+            finally
+            {
+                num1 = 0;
+                num2 = 0;
+            }
+        }
+    }
+}
