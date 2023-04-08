d3f517347e8c15c3608c89a2addd6ec02e90b017
“FeReDragon” <f3030ma@gmail.com>
2023-04-08 16:18:13 -0300

    EscolaDbContext

---
+++ Modulo_1/ex_S10/Escola_Bd_Api/Models/EscolaDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Escola_Bd_Api.Models;
+
+public class EscolaDbContext : DbContext
+{
+    public EscolaDbContext(DbContextOptions<EscolaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+    public DbSet<Discipline> Disciplines { get; set; }
+    public DbSet<StudentDiscipline> StudentDisciplines { get; set; }
+    public DbSet<Quiz> Quizzes { get; set; }
+    public DbSet<Question> Questions { get; set; }
+    public DbSet<Answer> Answers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+}
