e0328bd95853d569082eab09fe447d1628443c04
“FeReDragon” <f3030ma@gmail.com>
2023-06-18 21:39:07 -0300

    Ex angular 1,2 e 3

---
+++ Modulo_2/Ex_S7/exAngular_S7/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CalculadoraComponent,
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
