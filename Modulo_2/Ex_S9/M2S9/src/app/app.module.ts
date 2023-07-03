a96959ef66cb6275177183524cf650feb960b1fc
“FeReDragon” <f3030ma@gmail.com>
2023-07-03 10:20:17 -0300

    componentes: home, busca, header

--- Modulo_2/Ex_S9/M2S9/src/app/app.module.ts
+++ Modulo_2/Ex_S9/M2S9/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuscaComponent } from './busca/busca.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscaComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
