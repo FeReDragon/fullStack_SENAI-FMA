1ab0da28f003b1e0ce74f34cc09b7103d43ca51e
“FeReDragon” <f3030ma@gmail.com>
2023-07-10 17:50:26 -0300

    componente busca

--- Modulo_2/Ex_S9/M2S9/src/app/busca/busca.component.ts
+++ Modulo_2/Ex_S9/M2S9/src/app/busca/busca.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-busca',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./busca.component.scss']
 })
 export class BuscaComponent {
+  @Output() searchText = new EventEmitter<string>();
 
+  handleInput() {
+    this.searchText.emit('Valor do input');
+  }
 }
+
+
+
+
+
+
+
