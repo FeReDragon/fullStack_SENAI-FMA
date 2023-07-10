806de62ba3247ba4358609216b514fbe8a494909
“FeReDragon” <f3030ma@gmail.com>
2023-07-10 17:51:25 -0300

    rota e modulo

--- Modulo_2/Ex_S9/M2S9/src/app/app-routing.module.ts
+++ Modulo_2/Ex_S9/M2S9/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from './layouts/content/content.component';
 
-const routes: Routes = [];
+
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'signin', component: ContentComponent },
+  { path: 'esqueci-senha', component: ContentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Modulo_2/Ex_S9/M2S9/src/app/app.module.ts
+++ Modulo_2/Ex_S9/M2S9/src/app/app.module.ts
@@ -6,19 +6,30 @@ import { AppComponent } from './app.component';
 import { BuscaComponent } from './busca/busca.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms'; // Importe o módulo FormsModule
+import { FullComponent } from './layouts/full/full.component';
+import { LoginComponent } from './login/login.component';
+import { PublicoRoutingModule } from './components/publico/publico-routing.module';
+import { PrivadoRoutingModule } from './components/privado/privado-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     BuscaComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    FullComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    PublicoRoutingModule,
+    PrivadoRoutingModule, // Adicione FormsModule aqui
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
