5bcf03490db990548adc8848cb7e84cacb3268de
“FeReDragon” <f3030ma@gmail.com>
2023-07-10 17:28:19 -0300

    home atualizacao

--- Modulo_2/Ex_S7/exAngular_S7/src/app/home/home.component.ts
+++ Modulo_2/Ex_S7/exAngular_S7/src/app/home/home.component.ts
@@ -1,17 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  filmes: any[] = [];
+  loading: boolean = true;
+  currentIndex: number = 0;
+  totalFilmes: number = 0;
   texto= '';
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getFilmes().subscribe(
+      (response: any[]) => {
+        this.filmes = response;
+        this.totalFilmes = this.filmes.length;
+        this.loading = false;
+      },
+      (error: any) => {
+        console.error('Erro ao obter os filmes:', error);
+        this.loading = false;
+      }
+    );
+  }
+
+  onNext() {
+    if (this.currentIndex < this.totalFilmes - 1) {
+      this.currentIndex++;
+    }
+  }
+
+  onPrevious() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
 
   onButtonClick() {
     console.log('botao foi clicado');
   }
-
 }
+
