2b9a810478741348555648626e9086f694c88069
“FeReDragon” <f3030ma@gmail.com>
2023-07-10 17:27:43 -0300

    avalicao update

--- Modulo_2/Ex_S7/exAngular_S7/src/app/avaliacao-filme/avaliacao-filme.component.ts
+++ Modulo_2/Ex_S7/exAngular_S7/src/app/avaliacao-filme/avaliacao-filme.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-avaliacao-filme',
@@ -14,26 +15,39 @@ export class AvaliacaoFilmeComponent {
     avaliacao: new FormControl('', [Validators.required, Validators.min(1), Validators.max(10)])
   });
 
+  avaliacoesProntas: any[] = []; // Inicialize a propriedade com um array vazio
+
+  constructor(private apiService: ApiService) {
+  }
+
+  ngOnInit() {
+    this.apiService.get('avaliacoes-prontas').subscribe(
+      (response: any) => {
+        this.avaliacoesProntas = response;
+      },
+      (error: any) => {
+        console.error('Erro ao obter as avaliações prontas:', error);
+      }
+    );
+  }
+
   onSubmit() {
     if (this.avaliacaoForm.valid) {
-        const { nome, email, nomeFilme, avaliacao } = this.avaliacaoForm.value;
+      const { nome, email, nomeFilme, avaliacao } = this.avaliacaoForm.value;
 
-        console.log(`Nome: ${nome}`);
-        console.log(`Email: ${email}`);
-        console.log(`Nome do Filme: ${nomeFilme}`);
-        console.log(`Avaliação: ${avaliacao}`);
+      console.log(`Nome: ${nome}`);
+      console.log(`Email: ${email}`);
+      console.log(`Nome do Filme: ${nomeFilme}`);
+      console.log(`Avaliação: ${avaliacao}`);
 
-        console.log(`Você avaliou o filme ${nomeFilme}`);
-        this.avaliacaoForm.reset();
+      console.log(`Você avaliou o filme ${nomeFilme}`);
+      this.avaliacaoForm.reset();
     } else {
-        console.log('Formulário inválido');
+      console.log('Formulário inválido');
     }
-}
-
+  }
 
   onCancel() {
     this.avaliacaoForm.reset();
   }
-
-
 }
