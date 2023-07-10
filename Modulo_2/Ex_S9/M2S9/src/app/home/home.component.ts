e7ea5a0490257d061ab0529f213840d610443096
“FeReDragon” <f3030ma@gmail.com>
2023-07-10 17:50:05 -0300

    componente home

--- Modulo_2/Ex_S9/M2S9/src/app/home/home.component.ts
+++ Modulo_2/Ex_S9/M2S9/src/app/home/home.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  handleSearchText(value: string) {
+    console.log(value);
+  }
 }
+
