f563e9d8ad84cec2d029e8b3432022c1ab20d705
“FeReDragon” <f3030ma@gmail.com>
2023-08-01 17:59:37 -0300

    update pages component

--- Modulo_2/Ex_S10/Cardapio-Digital/src/app/pages/bebidas/bebidas.component.ts
+++ Modulo_2/Ex_S10/Cardapio-Digital/src/app/pages/bebidas/bebidas.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardapioService } from '../../cardapio.service';
+import { Bebida } from '../../shared/Interfaces/bebida.interface'; // Atualize este caminho para o caminho correto do arquivo de interface.
 
 @Component({
   selector: 'app-bebidas',
   templateUrl: './bebidas.component.html',
   styleUrls: ['./bebidas.component.scss']
 })
-export class BebidasComponent {
 
+export class BebidasComponent implements OnInit {
+  bebidas: Bebida[] = []; // Aqui, a variável bebidas foi declarada como um array de Bebida
+
+  constructor(private cardapioService: CardapioService) { }
+
+  ngOnInit(): void {
+    this.cardapioService.getBebidas().subscribe(data => {
+      this.bebidas = data;
+    });
+  }
 }
+
--- Modulo_2/Ex_S10/Cardapio-Digital/src/app/pages/comidas/comidas.component.ts
+++ Modulo_2/Ex_S10/Cardapio-Digital/src/app/pages/comidas/comidas.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardapioService } from '../../cardapio.service';
+import { Comida } from '../../shared/Interfaces/comida.interface';
 
 @Component({
   selector: 'app-comidas',
   templateUrl: './comidas.component.html',
   styleUrls: ['./comidas.component.scss']
 })
-export class ComidasComponent {
+export class ComidasComponent implements OnInit {
+  comidas: Comida[] = [];
 
+  constructor(private cardapioService: CardapioService) { }
+
+  ngOnInit(): void {
+    this.cardapioService.getComidas().subscribe(data => {
+      this.comidas = data;
+    });
+  }
 }
+
+
