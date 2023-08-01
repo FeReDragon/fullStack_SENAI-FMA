fe6707d234b25ded60c8a6d9401d91219739e7f6
“FeReDragon” <f3030ma@gmail.com>
2023-08-01 15:03:59 -0300

    criacao componentes e pages

--- Modulo_2/Ex_S10/Cardapio-Digital/src/app/app.module.ts
+++ Modulo_2/Ex_S10/Cardapio-Digital/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent } from './shared/components/home/home.component';
+import { ComidasComponent } from './pages/comidas/comidas.component';
+import { BebidasComponent } from './pages/bebidas/bebidas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ComidasComponent,
+    BebidasComponent
   ],
   imports: [
     BrowserModule,
