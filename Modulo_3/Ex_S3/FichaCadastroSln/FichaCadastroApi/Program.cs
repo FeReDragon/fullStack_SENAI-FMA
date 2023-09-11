0b4c523311145ca98ddb3ec80022188bc9085e41
“FeReDragon” <f3030ma@gmail.com>
2023-09-11 18:29:10 -0300

    update

--- Modulo_3/Ex_S3/FichaCadastroSln/FichaCadastroApi/Program.cs
+++ Modulo_3/Ex_S3/FichaCadastroSln/FichaCadastroApi/Program.cs
@@ -1,40 +1,69 @@
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FichaCadastroApi.HealthCheck;
 using FichaCadastroApi.Model;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Swashbuckle.AspNetCore.Swagger;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure Environment and Configuration
+var environmentName = builder.Environment.EnvironmentName;
 
+builder.Configuration
+       .SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+       .AddJsonFile($"appsettings.{environmentName}.json", optional: false, reloadOnChange: true);
+
+// Get the Connection String
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Configure Services
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<FichaCadastroDbContext>(options => 
+    options.UseSqlServer(connectionString)
+);
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "My API", Version = "v1" });
+});
 builder.Services.AddRouting(options =>
 {
     options.LowercaseUrls = true;
     options.LowercaseQueryStrings = true;
 });
-
-string connectionString = "Server=127.0.0.1,1433;Database=FichaCadastro;User Id=sa;Password=P@ssw0rd123!;TrustServerCertificate=True;";
-
-builder.Services.AddDbContext<FichaCadastroDbContext>(options => options.UseSqlServer(connectionString));
-
-builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddHealthChecks()
+       .AddCheck<HealthCheckCustom>(nameof(HealthCheckCustom));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure Middlewares and HTTP Request Pipeline
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
+app.UseHealthChecks("/healthcheck", new HealthCheckOptions()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+});
 
 app.Run();
